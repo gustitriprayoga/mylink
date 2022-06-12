6661ec05ac1bf81aa05dddbecb76366b9c2d874f
TukS <91913765+gustitriprayoga@users.noreply.github.com>
2022-06-12 19:26:38 +0700

    add new discord

--- src/assets/items.js
+++ src/assets/items.js
@@ -9,41 +9,48 @@ import twitter from '../assets/twitter.png'
 import saweria from '../assets/saweria.png'
 import sociabuzz from '../assets/sociabuzz.png'
 import facebook from '../assets/facebook.png'
+import discord from '../assets/discord.png'
 
 const items = [
 {
     "title": "Github",
-    "subtitle": "@gustitriprayoga | 🏡 Project Kecil-Kecilan ku",
+    "subtitle": "Click To My Github | My Small Project 🏡",
     "image": github,
     "link": "https://github.com/gustitriprayoga" //Github Profile link
 },
 {
     "title": "Instagram",
-    "subtitle": "@gustitriprayogabrx |  Galery Ke Dua 🙂",
+    "subtitle": "Click To My Instagram |  My Second Galery 🙂",
     "image": instagram,
     "link": "https://instagram.com/gustitriprayogabrx" //instagram profile link 
 },
 {
     "title": "YouTube",
-    "subtitle": "Tent Studios | Jarang Ke Pake ",
+    "subtitle": "Click To My Youtube | I Dont Know 🙂 ",
     "image": youtube,
     "link": "https://www.youtube.com/channel/UCdNNzLIXZZeXv9Tu94kFJTw"//youtube channel link 
 },
 {
     "title": "Facebook",
-    "subtitle": "@gustitriprayoga | Dari Tahun Penjajahan ",
+    "subtitle": "Click To My Facebook | My Wall In Facebook ",
     "image": facebook,
     "link": "https://www.facebook.com/gustitriprayoga/"//youtube channel link 
 },
 {
+    "title": "Disord",
+    "subtitle": "Click To Join My Discord | My Discord ",
+    "image": discord,
+    "link": "https://discord.gg/GxQ6B6y7Qj"//youtube channel link 
+},
+{
     "title": "Donate | Saweria",
-    "subtitle": "Yuk Suport Mengunakan Saweria !",
+    "subtitle": "Click To Donate Me In Saweria !",
     "image": saweria,
     "link": "https://saweria.co/Gustitriprayoga" //Telegram Pofile 
 },
 {
     "title": "Donate | Saweria",
-    "subtitle": "Bisa Suport Mulai Seribu Rupiah",
+    "subtitle": "Click To Donate Me In SociaBuzz For 1K",
     "image": sociabuzz,
     "link": "https://sociabuzz.com/atuxbrx/tribe" //Telegram Pofile 
 }]
